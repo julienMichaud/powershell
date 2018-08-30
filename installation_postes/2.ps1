63bf7840b3c747649b0c9df14109a9d37cd8ac22
julienMichaud <35925031+julienMichaud@users.noreply.github.com>
2018-08-30 09:57:04 +0200

    Add files via upload

---
+++ installation_postes/2.ps1
@@ -0,0 +1,12 @@
+﻿# Ajout du droit de modification sur le lecteur C pour "utilisateurs"
+$acl = get-acl -path “C:\”
+$new = “Utilisateurs”,”Modify”,”ContainerInherit,ObjectInherit”,”None”,”Allow”
+$accessRule = new-object System.Security.AccessControl.FileSystemAccessRule $new
+$acl.SetAccessRule($accessRule)
+$acl | Set-Acl “C:\”
+
+
+# Le prochain redémarrage se fera en mode normal 
+cmd.exe /c "bcdedit /deletevalue {default} safeboot"
+
+cmd.exe /c "shutdown /r"
