d2c40164cb2d603f9ece781e16155c640ad43bba
julienMichaud <35925031+julienMichaud@users.noreply.github.com>
2018-08-30 09:54:11 +0200

    Add files via upload

---
+++ bitlocker.ps1
@@ -0,0 +1,3 @@
+﻿Enable-BitLocker -MountPoint C: -EncryptionMethod XtsAes256   -UsedSpaceOnly -SkipHardwareTest -RecoveryPasswordProtector 
+
+(Get-BitLockerVolume -MountPoint c:).keyprotector.recoverypassword > "\\chemin_reseau\$env:computername.txt"
