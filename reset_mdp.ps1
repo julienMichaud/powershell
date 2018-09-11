c587f180664d7f4f9afc3e74d587158d1ea9a5b0
julienMichaud <35925031+julienMichaud@users.noreply.github.com>
2018-09-11 09:09:44 +0200

    Delete reset_mdp.ps1

--- reset_mdp.ps1
+++
@@ -1,9 +0,0 @@
-﻿Set-ExecutionPolicy RemoteSigned -force
-import-module activedirectory
-$usercred= read-host -prompt 'Entrez utilisateur  '
-$cred = Get-Credential $usercred
-$utilisateur= read-host -prompt 'Entrez le n° de CP'
-$newpwd = ConvertTo-SecureString -String "mot_de_pase_a_changer" -AsPlainText –Force
-Set-ADAccountPassword -credential $cred -identity $utilisateur -NewPassword $newpwd -Reset -PassThru | Set-ADuser -ChangePasswordAtLogon $True
-write-host "mdp de $utilisateur reinitialisé"
-Read-Host -Prompt "Appuyez sur Entrée pour quitter"
