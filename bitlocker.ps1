fe2b954982cab0f9eff841cbbb1f81fa10431ea0
julienMichaud <35925031+julienMichaud@users.noreply.github.com>
2018-09-11 09:08:05 +0200

    Delete bitlocker.ps1

--- bitlocker.ps1
+++
@@ -1,3 +0,0 @@
-﻿Enable-BitLocker -MountPoint C: -EncryptionMethod XtsAes256   -UsedSpaceOnly -SkipHardwareTest -RecoveryPasswordProtector 
-
-(Get-BitLockerVolume -MountPoint c:).keyprotector.recoverypassword > "\\chemin_reseau\$env:computername.txt"
